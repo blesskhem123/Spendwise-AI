bfe53045aa43e1f71af7f2563b31ac67f9de150e
Bless Khemchandani <blesskhem@gmail.com>
2026-08-21 15:38:38 +0530

    Fix Google Sign-In: wrap app in GoogleOAuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
+import { GOOGLE_CLIENT_ID } from './config/googleAuth'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
-
-
-
-
-
-
-
-
-
